02279b192a91d231eba0adafdf409a549b74b559
BAFO_ <wethinkcode_@pop-os.localdomain>
2024-04-29 12:41:22 +0200

    Landing Page Template

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,5 +1,7 @@
-import { connect } from "http2";
 import React from "react";
+import Hero from "./hero";
+import Features from "./features";
+import Footer from "./ui/footer";
 
 
 const ConnectButton = (props: {connect: Function}) => {
@@ -8,11 +10,33 @@ const ConnectButton = (props: {connect: Function}) => {
     );
 }
 
+{/* <ConnectButton  connect={props.connect} />     */}
+
+// return (
+//     <section className='bg-indigo-700 py-20 mb-4'>
+//       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+//         <div className='text-center'>
+//           <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
+//             {"Become a React Dev"}
+//           </h1>
+//           <p className='my-4 text-xl text-white'>{"Find the React job that fits your skill set"}</p>
+//           <ConnectButton  connect={props.connect} />
+//         </div>
+//       </div>
+//     </section>
+//   );
+
+
 
 const LandingPage = (props: {connect: Function}) => {
 
     return (
-        <ConnectButton  connect={props.connect} />    
+        <>
+        <Hero connect={props.connect}/>
+        <Features />
+        {/* <Footer /> */}
+    
+      </>
     );
 }
 
